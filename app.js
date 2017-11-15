9b3f009601a6a5129ed041a9b459504687c80a3e
Alexander <andresa@hochschule-trier.de>
2017-11-15 12:07:23 +0100

    change

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-// setup ports
+// setup ports jaaaaaaaaaaaaaaaaaaaaa
+//toll
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
